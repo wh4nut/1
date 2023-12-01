07f08adb7e294ae84a8de4da47184dce2dd2395d
Kyrylo Sudakov <wh4nut@gmail.com>
2023-12-01 02:54:51 +0200

    add

--- app.js
+++ app.js
@@ -1,19 +1,107 @@
-const numberOfFilms = +prompt('How much films do you see before?', '');
+// const numberOfFilms = +prompt('How much films do you see before?', '');
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-};
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false,
+// };
 
-const lastFilmQuestion = prompt('Name of last film?', ''),
-	filmRateQuestion = +prompt('Score of this film from 1 o 10', ''),
-	lastFilmQuestion2 = prompt('Name of last film?', ''),
-	filmRateQuestion2 = +prompt('Score of this film from 1 o 10', '');
+// const lastFilmQuestion = prompt('Name of last film?', ''),
+// 	filmRateQuestion = +prompt('Score of this film from 1 o 10', ''),
+// 	lastFilmQuestion2 = prompt('Name of last film?', ''),
+// 	filmRateQuestion2 = +prompt('Score of this film from 1 o 10', '');
 
-personalMovieDB.movies[lastFilmQuestion] = filmRateQuestion;
-personalMovieDB.movies[lastFilmQuestion2] = filmRateQuestion2;
+// personalMovieDB.movies[lastFilmQuestion] = filmRateQuestion;
+// personalMovieDB.movies[lastFilmQuestion2] = filmRateQuestion2;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if (1) {
+// 	console.log('Ok!');
+// } else {
+// 	console.error('error! Wrong');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+// 	console.log('Error!');
+// } else if (num > 100) {
+// 	console.log('Too much!');
+// } else {
+// 	console.log('Ok!');
+// }
+
+// num === 50 ? console.log('okey!') : console.log('Error!');
+
+// switch (num) {
+// 	case 49:
+// 		console.log('No');
+// 		break;
+// 	case 60:
+// 		console.log('No');
+// 		break;
+// 	case 50:
+// 		console.log('Yes!');
+// 		break;
+// 	default:
+// 		console.log('Not now...');
+// 		break;
+// }
+
+// const gamburger = 2;
+// const fries = 1;
+
+// if (gamburger === 3 && fries) {
+// 	console.log('I"m not hungry');
+// } else {
+// 	console.log('I"m hungry');
+// }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// for (let i = 1; i < 7; i++) {
+// 	let result = '';
+// 	for (let j = 0; j < i; j++) {
+// 		result += '*';
+// 	}
+
+// 	console.log(result);
+// }
+
+// first: for (let i = 1; i < 3; i++) {
+// 	for (let j = 0; j < 3; j++) {
+// 		for (let k = 0; k < 3; k++) {
+// 			if (k === 2) {
+// 				continue first;
+// 			}
+// 		}
+// 	}
+// }
